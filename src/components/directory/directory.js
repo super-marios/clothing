3e1890eadc476b5b3820b2f8533fd0f3b628d792
wootangg <wjckorea001@gmail.com>
2020-06-06 22:49:10 +0900

    added items

---
+++ src/components/directory/directory.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./directory.styles.scss";
+
+import MenuItem from "../menu-item/menu-item";
+
+let data = [
+  {
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    id: 1,
+    linkUrl: "hats",
+  },
+  {
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    id: 2,
+    linkUrl: "",
+  },
+  {
+    title: "sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    id: 3,
+    linkUrl: "",
+  },
+  {
+    title: "womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    size: "large",
+    id: 4,
+    linkUrl: "",
+  },
+  {
+    title: "mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    size: "large",
+    id: 5,
+    linkUrl: "",
+  },
+];
+
+const Directory = () => {
+  const [sections, setSections] = useState(data);
+
+  return (
+    <div className="directory-menu">
+      {sections.map(({ title, imageUrl, id, size }) => {
+        return (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Directory;

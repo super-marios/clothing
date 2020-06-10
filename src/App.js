baf924bc6daaa74f1b78ee602233f5005dcda3b0
wootangg <wjckorea001@gmail.com>
2020-06-10 23:38:27 +0900

    added dynamic router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 
+import { Switch, Route, Link } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage";
 
+const HatsPage = () => {
+  return (
+    <div>
+      {/* <Link to="/topics">Topics</Link> */}
+      <h1>Hats PAge</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -43,10 +43,8 @@ const Directory = () => {
 
   return (
     <div className="directory-menu">
-      {sections.map(({ title, imageUrl, id, size }) => {
-        return (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-        );
+      {sections.map(({ id, ...otherSectionProps }) => {
+        return <MenuItem key={id} {...otherSectionProps} />;
       })}
     </div>
   );

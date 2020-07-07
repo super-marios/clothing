c135432f2fca761eec6e668c4e4d0457f7d056dc
super-marios <wjckorea001@gmail.com>
2020-07-08 00:10:01 +0900

    added sign out link

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <div>
-      <Header />
+      <Header currentUser={currentUser} />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -62,11 +62,12 @@ const SignIn = () => {
           handleChange={handleChange}
           label="password"
         />
-
-        <CustomButton type="submit">Sign In</CustomButton>
-        <CustomButton onClick={signInWithGoogle}>
-          Sign In with Google
-        </CustomButton>
+        <div className="buttons">
+          <CustomButton type="submit">Sign In</CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            Sign In with Google
+          </CustomButton>
+        </div>
       </form>
     </div>
   );

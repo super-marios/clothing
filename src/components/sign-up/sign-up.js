0df355538645aeb9921134530086d62b9e2c7fdf
super-marios <wjckorea001@gmail.com>
2020-07-17 00:30:34 +0900

    added signin with email

---
+++ src/components/sign-up/sign-up.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+
+import FormInput from "../form-input/form-input";
+import CustomButton from "../custom-button/custom-button";
+
+import { auth, createUserProfileDocument } from "../firebase/firebase.utils";
+
+import "./sign-up.scss";
+
+const SignUp = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("paswords don't match");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserProfileDocument(user, displayName);
+
+      setDisplayName("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === "displayName") {
+      setDisplayName(value);
+    }
+
+    if (name === "email") {
+      setEmail(value);
+    }
+    if (name === "password") {
+      setPassword(value);
+    }
+
+    if (name === "confirmPassword") {
+      setConfirmPassword(value);
+    }
+  };
+
+  return (
+    <div className="sign-up">
+      <h2 className="title">I do not have an account</h2>
+      <span>Sign up with your email and password</span>
+      <form className="sign-up-form" onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          name="displayName"
+          value={displayName}
+          onChange={handleChange}
+          label="Display Name"
+          required
+        ></FormInput>
+        <FormInput
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          label="Email"
+          required
+        ></FormInput>
+        <FormInput
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          label="Password"
+          required
+        ></FormInput>
+        <FormInput
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={handleChange}
+          label="Confirm Password"
+          required
+        ></FormInput>
+        <CustomButton type="submit">SIGN UP</CustomButton>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
--- src/pages/signin-and-signup/signin-and-signup.js
+++ src/pages/signin-and-signup/signin-and-signup.js
@@ -3,11 +3,13 @@ import React from "react";
 import "./signin-and-signup.scss";
 
 import SignIn from "../../components/sign-in/sign-in";
+import SignUp from "../../components/sign-up/sign-up";
 
 const SignInAndSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
